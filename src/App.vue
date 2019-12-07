175ead9e9b2c8b8bee89258a49080b5bbeafca4d
bicen.zhu <bicen.zhu@nokia-sbell.com>
2019-12-07 21:19:34 +0800

    Extract topbar and navdrawer

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,8 @@
 <template>
     <v-app id="inspire">
-      <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
-        <v-list dense>
-          <template v-for="item in items">
-            <v-layout v-if="item.heading" :key="item.heading" row align-center>
-              <v-flex xs6>
-                <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
-              </v-flex>
-              <v-flex xs6 class="text-xs-center">
-                <a href="#!" class="body-2 black--text">EDIT</a>
-              </v-flex>
-            </v-layout>
-            <v-list-group
-              v-else-if="item.children"
-              :key="item.text"
-              v-model="item.model"
-              :prepend-icon="item.model ? item.icon : item['icon-alt']"
-              append-icon
-            >
-              <template v-slot:activator>
-                <v-list-item>
-                  <v-list-item-content>
-                    <v-list-item-title>{{ item.text }}</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </template>
-              <v-list-item v-for="(child, i) in item.children" :key="i" @click.stop>
-                <v-list-item-action v-if="child.icon">
-                  <v-icon>{{ child.icon }}</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title>{{ child.text }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-group>
-            <v-list-item v-else :key="item.text" @click.stop>
-              <v-list-item-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>{{ item.text }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-        </v-list>
-      </v-navigation-drawer>
+      <NavDrawer />
+      <TopBar />
 
-      <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
-        <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-          <span class="hidden-sm-and-down">Google Contacts</span>
-        </v-toolbar-title>
-        <v-text-field
-          flat
-          solo-inverted
-          hide-details
-          prepend-inner-icon="search"
-          label="Search"
-          class="hidden-sm-and-down"
-        ></v-text-field>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>apps</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>notifications</v-icon>
-        </v-btn>
-        <v-btn icon large>
-          <v-avatar size="32px" item>
-            <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify"></v-img>
-          </v-avatar>
-        </v-btn>
-      </v-app-bar>
       <v-content>
         <v-container fluid fill-height>
           <v-layout align-center justify-center>
@@ -146,47 +77,19 @@
 
 <script>
 
+import NavDrawer from "./components/NavDrawer"
+import TopBar from "./components/TopBar"
 export default {
   name: "App",
 
   components: {
+    NavDrawer,
+    TopBar
   },
 
   data: () => ({
     dialog: false,
-    drawer: null,
-    items: [
-      { icon: 'contacts', text: 'Contacts' },
-      { icon: 'history', text: 'Frequently contacted' },
-      { icon: 'content_copy', text: 'Duplicates' },
-      {
-        icon: 'keyboard_arrow_up',
-        'icon-alt': 'keyboard_arrow_down',
-        text: 'Labels',
-        model: true,
-        children: [
-          { icon: 'add', text: 'Create label' },
-        ],
-      },
-      {
-        icon: 'keyboard_arrow_up',
-        'icon-alt': 'keyboard_arrow_down',
-        text: 'More',
-        model: false,
-        children: [
-          { text: 'Import' },
-          { text: 'Export' },
-          { text: 'Print' },
-          { text: 'Undo changes' },
-          { text: 'Other contacts' },
-        ],
-      },
-      { icon: 'settings', text: 'Settings' },
-      { icon: 'chat_bubble', text: 'Send feedback' },
-      { icon: 'help', text: 'Help' },
-      { icon: 'phonelink', text: 'App downloads' },
-      { icon: 'keyboard', text: 'Go to the old version' },
-    ],
+    drawer: null
   }),
 };
 </script>
